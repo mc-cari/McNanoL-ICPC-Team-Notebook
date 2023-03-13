159ad7ef300a796870712fe5ecf99f9e745f537d
MarceloL <55332694+Marceantasy@users.noreply.github.com>
2023-03-13 13:00:57 -0300

    Update MaxFlowDinic.cpp

--- Graphs/MaxFlowDinic.cpp
+++ Graphs/MaxFlowDinic.cpp
@@ -16,29 +16,23 @@
 
 // maximo matching m*sqrt(n)
 
-class Dinic
-{
+class Dinic{
     struct Edge { int to, rev; ll f, c; };
     int n, t_; vector<vector<Edge>> G;
     vl D; vi q, W;
-
-    bool bfs(int s, int t)
-    {
+    bool bfs(int s, int t){
         W.assign(n, 0); D.assign(n, -1); D[s] = 0;
         int f = 0, l = 0; q[l++] = s;
-        while (f < l)
-        {
+        while (f < l){
             int u = q[f++];
             for (const Edge &e : G[u]) if (D[e.to] == -1 && e.f < e.c)
                 D[e.to] = D[u] + 1, q[l++] = e.to;
         }
         return D[t] != -1;
     }
-    ll dfs(int u, ll f)
-    {
+    ll dfs(int u, ll f){
         if (u == t_) return f;
-        for (int &i = W[u]; i < (int)G[u].size(); ++i)
-        {
+        for (int &i = W[u]; i < (int)G[u].size(); ++i){
             Edge &e = G[u][i]; int v = e.to;
             if (e.c <= e.f || D[v] != D[u] + 1) continue;
             ll df = dfs(v, min(f, e.c - e.f));
@@ -49,13 +43,11 @@ class Dinic
 
 public:
     Dinic(int N) : n(N), G(N), D(N), q(N) {}
-    void addEdge(int u, int v, ll cap)
-    {
+    void addEdge(int u, int v, ll cap){
         G[u].push_back({v, (int)G[v].size(), 0, cap});
         G[v].push_back({u, (int)G[u].size() - 1, 0, 0}); // cap if bidirectional
     }
-    ll maxFlow(int s, int t)
-    {
+    ll maxFlow(int s, int t){
         t_ = t; ll ans = 0;
         while (bfs(s, t)) while (ll dl = dfs(s, LLONG_MAX)) ans += dl;
         return ans;
