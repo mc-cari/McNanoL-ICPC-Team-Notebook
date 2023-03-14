85c4bac95d2bd6229582d1f47e8e8801cdb03bdc
MarceloL <55332694+Marceantasy@users.noreply.github.com>
2023-03-14 01:07:51 -0300

    Update SegmentTreeIterative.cpp

--- Structures/SegmentTreeIterative.cpp
+++ Structures/SegmentTreeIterative.cpp
@@ -1,7 +1,6 @@
 #include "../Header.cpp"
 
-struct Node
-{
+struct Node{
     int v;
     Node() { v = 0; } // neutro
     Node(int v) : v(v) {}
@@ -10,25 +9,19 @@ struct Node
 
 // 0 - indexed / inclusive - exclusive
 template <class node>
-struct ST
-{
+struct ST{
     vector<node> t; int n;
-
-    ST(vector<node> &arr, int N) : n(N), t(N * 2)
-    {
+    ST(vector<node> &arr, int N) : n(N), t(N * 2){
         copy(arr.begin(), arr.end(), t.begin() + n);
         for (int i = n - 1; i > 0; --i) t[i] = node(t[i << 1], t[i << 1 | 1]);
     }
-    void set(int p, const node &value)
-    {
+    void set(int p, const node &value){
         for (t[p += n] = value; p >>= 1;)
             t[p] = node(t[p << 1], t[p << 1 | 1]);
     }
-    node query(int l, int r)
-    {
+    node query(int l, int r){
         node ansl, ansr;
-        for (l += n, r += n; l < r; l >>= 1, r >>= 1)
-        {
+        for (l += n, r += n; l < r; l >>= 1, r >>= 1){
             if (l & 1) ansl = node(ansl, t[l++]);
             if (r & 1) ansr = node(t[--r], ansr);
         }
