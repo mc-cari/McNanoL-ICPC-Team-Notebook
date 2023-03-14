e870823eac400700dd935a49378da42a0c86e748
MarceloL <55332694+Marceantasy@users.noreply.github.com>
2023-03-14 01:20:42 -0300

    Update LongestCommonSubstring.cpp

--- Strings/LongestCommonSubstring.cpp
+++ Strings/LongestCommonSubstring.cpp
@@ -1,7 +1,6 @@
 #include "../Header.cpp"
 
-int LCSubStr(string& X, string& Y, int m, int n)
-{
+int LCSubStr(string& X, string& Y, int m, int n){
     // Create a table to store lengths of longest common suffixes of
     // substrings.   Notethat LCSuff[i][j] contains length of longest
     // common suffix of X[0..i-1] and Y[0..j-1]. The first row and
@@ -9,17 +8,12 @@ int LCSubStr(string& X, string& Y, int m, int n)
     // for simplicity of program
     int LCSuff[m+1][n+1];
     int result = 0;  // To store length of the longest common substring
-
     /* Following steps build LCSuff[m+1][n+1] in bottom up fashion. */
-    for (int i=0; i<=m; i++)
-    {
-        for (int j=0; j<=n; j++)
-        {
+    for (int i=0; i<=m; i++){
+        for (int j=0; j<=n; j++){
             if (i == 0 || j == 0)
                 LCSuff[i][j] = 0;
-
-            else if (X[i-1] == Y[j-1])
-            {
+            else if (X[i-1] == Y[j-1]){
                 LCSuff[i][j] = LCSuff[i-1][j-1] + 1;
                 result = max(result, LCSuff[i][j]);
             }
@@ -29,14 +23,9 @@ int LCSubStr(string& X, string& Y, int m, int n)
     return result;
 }
 
-int main()
-{
+int main(){
     string X = "OldSite:GeeksforGeeks.org", Y = "NewSite:GeeksQuiz.com";
-
-
-    int m = X.size();
-    int n = Y.size();
-
+    int m = X.size(), n = Y.size();
     cout << "Length of Longest Common Substring is " << LCSubStr(X, Y, m, n) << "\n";
     return 0;
 }
