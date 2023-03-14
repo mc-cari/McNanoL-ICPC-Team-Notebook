8812ae142bb9e23fe797f591d7a1e385a3adfff4
MarceloL <55332694+Marceantasy@users.noreply.github.com>
2023-03-14 01:07:04 -0300

    Update DinamicConnectivity.cpp

--- Structures/DinamicConnectivity.cpp
+++ Structures/DinamicConnectivity.cpp
@@ -2,30 +2,21 @@
 #include "UnionFind.cpp"
 
 // M log M (log M from UF)
-struct DinC
-{
+struct DinC{
 	vector<vp> DC;
 	ll T; UF uf;
-	DinC(ll times, ll n)
-	{
+	DinC(ll times, ll n){
 		T = times;
 		DC.assign(4*T, vp());
 		uf = UF(n);
 	}
-	void qry()
-	{
+	void qry(){
 		qry(1, 0, T-1);
 	}
-	void qry(ll n, ll l, ll r)
-	{
-
+	void qry(ll n, ll l, ll r){
 		for(auto it : DC[n]) uf.join(it.first, it.second);
-
-		if(l == r) // process time l
-		{
-			for(auto it : ord[l])
-			{
-				
+		if(l == r){ // process time l
+			for(auto it : ord[l]){
 				ll x1 = uf.find(it.first);
 				ll x2 = uf.find(it.second);
 				ans += uf.sz[x1] * uf.sz[x2];
@@ -36,21 +27,17 @@ struct DinC
 			qry(2*n,l,(l+r)/2);
 			qry(2*n+1,(l+r)/2+1,r);
 		}
-
 		for(auto it : DC[n]) uf.rollback();
 	}
-	void upd(ll i,ll j, pll v)
-	{
+	void upd(ll i,ll j, pll v){
 		return upd(1, 0, T-1, i, j, v);
 	}
-	void upd(ll n, ll l, ll r, ll i, ll j, pll v)
-	{
+	void upd(ll n, ll l, ll r, ll i, ll j, pll v){
 		if(r < i || j < l) return;
-        if(i <= l && r <= j)
-        {
-            DC[n].pb(v);
-            return;
-        }
+		if(i <= l && r <= j){
+		    DC[n].pb(v);
+		    return;
+		}
 		upd(2*n,l,(l+r)/2,i,j,v);
 		upd(2*n+1,(l+r)/2+1,r,i,j,v);
 	}
