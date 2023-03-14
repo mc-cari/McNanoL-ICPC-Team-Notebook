47ec76fa0579aa52167a2ef157335417ff31b970
MarceloL <55332694+Marceantasy@users.noreply.github.com>
2023-03-14 01:16:09 -0300

    Update AhoCorasick.cpp

--- Strings/AhoCorasick.cpp
+++ Strings/AhoCorasick.cpp
@@ -7,12 +7,10 @@ struct AC
     // L -> suffix link G -> anti L
     // E -> string finish
     AC (ll M, ll A) : M(M), A(A), N(M, vl(A, 0)), G(M, vl()), E(M, 0), L(M, 0), val(M, 0) {}
-    ll add(string s) // return endpoint
-    {
+    ll add(string s){ // return endpoint
         af++;
         ll p = 0;
-        for (char l : s)
-        {
+        for (char l : s){
             int t = l - 'a';
             if (!N[p][t]) N[p][t] = ++c;
             p = N[p][t];
@@ -20,23 +18,16 @@ struct AC
         val[p] = 1;
         return p;
     }
-    void init()
-    {
-
+    void init(){
         queue<int> q; q.push(0); L[0] = -1;
-        while (!q.empty())
-        {
+        while (!q.empty()){
             int p = q.front(); q.pop();
-            for(int c = 0; c < A; c++)
-            {
+            for(int c = 0; c < A; c++){
                 int u = N[p][c]; if (!u) continue;
                 L[u] = L[p] == -1 ? 0 : N[L[p]][c], q.push(u);
-
                 G[L[u]].push_back(u);
             }
             if (p) for(int c = 0; c < A; c++) if (!N[p][c]) N[p][c] = N[L[p]][c];
         }
-        
     }
 };
-
