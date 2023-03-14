bd6615b8eec582dff8617d64af9523695c922d81
MarceloL <55332694+Marceantasy@users.noreply.github.com>
2023-03-14 01:40:44 -0300

    Update SparseTable.cpp

--- Structures/SparseTable.cpp
+++ Structures/SparseTable.cpp
@@ -1,32 +1,24 @@
 #include "../Header.cpp"
 
-struct SparseTable
-{
+struct SparseTable{
     vector<vl >SP;
-	
-    SparseTable(vl&A)
-    {
+    SparseTable(vl&A){
         int n = A.size();
         SP.push_back(A);
         ll maxlog = 31 - __builtin_clz(n);
-        repx(i, 1 ,maxlog+1)
-		{
-			vl aux;
-			rep(j, n-(1<<i)+1)
-            {
+        repx(i, 1 ,maxlog+1){
+	    vl aux;
+	    rep(j, n-(1<<i)+1){
                 aux.push_back(max(SP[i-1][j],SP[i-1][j+(1<<(i-1))]));
             }
             SP.push_back(aux);
         }
-
     }
-    ll op(int l, int r)
-    {
+    ll op(int l, int r){
         ll maxlog = 31 - __builtin_clz(r-l+1);
         return max(SP[maxlog][l],SP[maxlog][r-(1<<maxlog)+1]);
     }
-    ll find(int l, int r, ll m) // maxi
-    {
+    ll find(int l, int r, ll m){ // maxi
       ll maxlog = 31 - __builtin_clz(r-l+1);
       for(int i = maxlog; i >= 0; i--){
         if(l + (1<<i) <= r && SP[i][l] < m){
@@ -37,12 +29,9 @@ struct SparseTable
     }
 };
 
-int main()
-{
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	ll n,m,x,y,h,v;
-
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    ll n,m,x,y,h,v;
     vector<int>A = {7,10,19,3,1,2,7,7};
     SparseTable SP(A);
     cout<<SP.op(1,3)<<endl;
