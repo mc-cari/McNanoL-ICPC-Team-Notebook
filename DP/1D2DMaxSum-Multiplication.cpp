dc7ec1fdd1a6789f92319c8cab3c93ea33e7d7fe
MarceloL <55332694+Marceantasy@users.noreply.github.com>
2023-03-14 01:49:59 -0300

    Update 1D2DMaxSum-Multiplication.cpp

--- DP/1D2DMaxSum-Multiplication.cpp
+++ DP/1D2DMaxSum-Multiplication.cpp
@@ -1,33 +1,27 @@
 #include "../Header.cpp"
 
-int main()
-{
+int main(){
     // 1D Max Array Sum
     int n = 9, A[] = { 4, -5, 4, -3, 4, 4, -4, 4, -5 }; //Allow all negative numbers
     int sum = A[0], ans = A[0];
-    for (int i = 1; i < n; i++)
-    {
+    for (int i = 1; i < n; i++){
         sum = max(A[i] + sum, A[i]); // Ignores sum if prev sum is worse than A[i]
         ans = max(ans, sum);
     }
     cout << ans << "\n";
-
     // 2D Max Array Sum
     int B [100][100];
     ans = -INF;
     cin>>n;
-    for (int i = 0; i < n; i++) for (int j = 0; j < n; j++)
-    {
+    for (int i = 0; i < n; i++) for (int j = 0; j < n; j++){
         cin >> B[i][j];
         if (j > 0) B[i][j] += B[i][j-1]; // Acum sum per Row
     }
-    for (int l = 0; l < n; l++) for (int r = l; r < n; r++)
-    {
+    for (int l = 0; l < n; l++) for (int r = l; r < n; r++){
         sum = B[0][r];
         int SubAns = B[0][r];
         if (l > 0){ sum -= B[0][l-1]; SubAns -= B[0][l-1]; }
-        for (int row = 1; row < n; row++)
-        {
+        for (int row = 1; row < n; row++){
             int aux = B[row][r];
             if(l > 0) aux -= B[row][l-1];
             sum = max(sum + aux, aux);
@@ -36,27 +30,22 @@ int main()
         ans = max(ans, SubAns);
     }
     cout << ans << "\n";
-
+    
     // Max Array Multiplication
     vl c;
     bool o = 0;
     ans = 1;
     ll miniend = 1, maxiend = 1;
-    for (int i = 0; i < c.size(); i++)
-    {
-        if(c[i] > 0)
-        {
+    for (int i = 0; i < c.size(); i++){
+        if(c[i] > 0){
             o = 1;
             if(miniend < 0)miniend *= c[i];
             maxiend *= c[i];
         }
-        else if(c[i] == 0)
-        {
+        else if(c[i] == 0){
             miniend = 1;
             maxiend = 1;
-        }
-        else
-        {
+        }else{
             int aux = maxiend;
             maxiend = max(1LL, miniend * c[i]);
             miniend = aux*c[i];
@@ -64,11 +53,9 @@ int main()
         ans = max(ans, maxiend);
     }
     if(ans == 1 && !o) cout << "0\n";
-    else
-    {
+    else{
         cout << ans << "\n";
     }
-
 
     // n dimension acumulative sum
     for dim = 0 to 4
@@ -82,5 +69,4 @@ int main()
                             dp(a, b, c, d) += dp (pa, pb, pc, pd)
     // o por cada celda
     for x in S //(celda de menor a mayor tal que todas las anteriores estan procesadas)
-
 }
