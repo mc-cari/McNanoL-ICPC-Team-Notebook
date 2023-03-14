6ff57d6223bc9efce25b0b9de7eaa774d9cf7002
MarceloL <55332694+Marceantasy@users.noreply.github.com>
2023-03-14 02:01:10 -0300

    Update Dijkstra.cpp

--- Graphs/Dijkstra.cpp
+++ Graphs/Dijkstra.cpp
@@ -1,26 +1,21 @@
 #include "../Header.cpp"
 
-int main()
-{
-	vl d(v, INF);
-	priority_queue<pll, vp, greater<pll> > q; //from low to high
-	ll s, t;
-
-	q.push({0, s});
-	d[s] = 0;	
-	while(!q.empty()){
-		ll w, u;
-		tie(w, u) = q.top();
-	    q.pop();
-		
-		if(w > d[u])continue;
-		for(auto it : g[u])
-		{
-		   	if(d[it.second] > w + it.first)
-		   	{
-		   		d[it.second] = w + it.first;
-		   		q.push({d[it.second], it.second});
-			}
-	  }
-	}
+int main(){
+    vl d(v, INF);
+    priority_queue<pll, vp, greater<pll> > q; //from low to high
+    ll s, t;
+    q.push({0, s});
+    d[s] = 0;	
+    while(!q.empty()){
+        ll w, u;
+        tie(w, u) = q.top();
+        q.pop();
+        if(w > d[u]) continue;
+        for(auto it : g[u]){
+	    if(d[it.second] > w + it.first){
+	        d[it.second] = w + it.first;
+	        q.push({d[it.second], it.second});
+	    }
+        }
+    }
 }
