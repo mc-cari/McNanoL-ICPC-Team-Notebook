b6be2e2463d567591c54ce18fe9fb602330f4f63
MarceloL <55332694+Marceantasy@users.noreply.github.com>
2023-03-14 15:02:37 -0300

    Update BinarySearch-Ternary.cpp

--- Structures/BinarySearch-Ternary.cpp
+++ Structures/BinarySearch-Ternary.cpp
@@ -1,22 +1,16 @@
 #include "../Header.cpp"
-
 // limit
 log(valor maximo/precision)/log(2)
                             /log(3/2) //para ternaria
 
 ll l = 0, r = n-1, res = -1;
-while(l <= r)
-{
+while(l <= r){
     ll p = (l + r) / 2;
     if(c[p] <= m)l = p+1;
     else r = p-1;
 }
-
-
 db l = 0, r = PI/2, mini = 1e10;
-
-rep(i, 101)
-{
+rep(i, 101){
     db d=(r-l)/3.0,m1=l+d,m2=r-d;
     db c1 = value(m1), c2 = value(m2);
     // Para el maximo cambiar r-m2 con l-m1
@@ -27,8 +21,7 @@ rep(i, 101)
 
 // non continuous
 ll = 1,r = m, mini = 1e17;
-while(l <= r)
-{
+while(l <= r){
     ll d = (r-l)/3;
     ll m1 = l+d, m2 = r-d;
     ll c1 = value(m1), c2 = value(m2);
