bf0aa4d092a196720797a93eb71c124a54a5608e
MarceloL <55332694+Marceantasy@users.noreply.github.com>
2023-03-14 01:03:15 -0300

    Update SegmentTreePersistent.cpp

--- Structures/SegmentTreePersistent.cpp
+++ Structures/SegmentTreePersistent.cpp
@@ -4,8 +4,7 @@ struct SegmentTree
 {
 	vector<vl> ST; vector<vi> Leftv, Rightv;
 	int N;
-	SegmentTree(vl &A)
-	{
+	SegmentTree(vl &A){
 		N = A.size();
 		ST.resize(4*N, vl());
 		// ST.resize(4*N, vl(1, 0)); whitout build, init with 0s
@@ -14,48 +13,32 @@ struct SegmentTree
 		bd(1,0,N-1,A);
 	}
 	ll op(ll x, ll y) { return min(x,y); }
-	void bd(int n, int l, int r, vl &A)
-	{
-		if(l==r)
-		{
+	void bd(int n, int l, int r, vl &A){
+		if(l==r){
 			ST[n].push_back(A[r]);
 			return;
 		}
-
 		bd(2*n,l,(l+r)/2,A);
 		bd(2*n+1,(l+r)/2+1,r,A);
-
 		ST[n].push_back(op(ST[2*n][0], ST[2*n+1][0]));
 	}
-	ll qry(int i, int j, int vs)
-	{
+	ll qry(int i, int j, int vs){
 		return qry(1,0,N-1,i,j, vs);
 	}
-	ll qry(int n, int l, int r, int i, int j, int vs)
-	{
-
+	ll qry(int n, int l, int r, int i, int j, int vs){
 		if(r < i || j < l) return 0;
-
 		if(i <= l && r <= j) return ST[n][vs];
-
 		return op(qry(2*n,l,(l+r)/2,i,j, Leftv[n][vs]), qry(2*n+1,(l+r)/2+1,r,i,j, Rightv[n][vs]));
 	}
-
-	void upd(int i, ll v)
-	{
+	void upd(int i, ll v){
 		return upd(1,0,N-1,i,v);
 	}
-	void upd(int n, int l, int r, int i, ll v)
-	{
-
+	void upd(int n, int l, int r, int i, ll v){
 		if(i < l || r < i) return;
-
-		if(l == r)
-		{
+		if(l == r){
 			ST[n].push_back(v); // ST[n].push_back(v + ST[n].back()) add
 			return;
 		}
-
 		upd(2*n,l,(l+r)/2,i,v);
 		upd(2*n+1,(l+r)/2+1,r,i,v);
 
