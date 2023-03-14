bae9eca826ede21f96f36b8168fdce6797eb8865
MarceloL <55332694+Marceantasy@users.noreply.github.com>
2023-03-14 01:35:57 -0300

    Update SegmentTree.cpp

--- Structures/SegmentTree.cpp
+++ Structures/SegmentTree.cpp
@@ -1,18 +1,14 @@
 #include "../Header.cpp"
-struct SegmentTree
-{
+struct SegmentTree{
 	vl ST; int N;
-	SegmentTree(vl &A)
-	{
+	SegmentTree(vl &A){
 		N = A.size();
 		ST.assign(4*N, 0);
 		bd(1,0,N-1,A);
 	}
 	ll op(ll x, ll y) { return min(x,y); }
-	void bd(int n, int l, int r, vl &A)
-	{
-		if(l == r)
-		{
+	void bd(int n, int l, int r, vl &A){
+		if(l == r){
 			ST[n] = A[r];
 			return;
 		}
@@ -20,36 +16,25 @@ struct SegmentTree
 		bd(2*n+1,(l+r)/2+1,r,A);
 		ST[n] = op(ST[2*n], ST[2*n+1]);
 	}
-	ll qry(int i, int j)
-	{
+	ll qry(int i, int j){
 		return qry(1,0,N-1,i,j);
 	}
-	ll qry(int n, int l, int r, int i, ll j)
-	{
+	ll qry(int n, int l, int r, int i, ll j){
 		if(r < i || j < l) return 0;
 		if(i <= l && r <= j) return ST[n];
-
 		return op(qry(2*n,l,(l+r)/2,i,j), qry(2*n+1,(l+r)/2+1,r,i,j));
 	}
-	void upd(int i, ll v)
-	{
+	void upd(int i, ll v){
 		return upd(1,0,N-1,i,v);
 	}
-	void upd(int n, int l, int r, int i, ll v)
-	{
+	void upd(int n, int l, int r, int i, ll v){
 		if(i < l || r < i) return;
-		if(l == r)
-		{
+		if(l == r){
 			ST[n] = v;
 			return;
 		}
-
 		upd(2*n,l,(l+r)/2,i,v);
 		upd(2*n+1,(l+r)/2+1,r,i,v);
 		ST[n] = op(ST[2*n], ST[2*n+1]);
 	}
 };
-
-
-
-
