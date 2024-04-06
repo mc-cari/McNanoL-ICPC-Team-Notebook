b77248f7197b37633f0004f2538c630c5a404405
IgnAMS <nanomonardes123@gmail.com>
2024-04-06 13:25:17 -0300

    fix: Benja Hash fixed

--- Strings/Hashing.cpp
+++ Strings/Hashing.cpp
@@ -1,25 +1,37 @@
 #include "../Header.cpp"
-struct RH
-{
+
+struct RH {
+    int binpow(int a, int b, int MOD) {
+        int res = 1;
+        while(b) {
+            if(b & 1) res = (1LL * res * a) % MOD;
+            a = (1LL * a * a) % MOD;
+            b >>= 1;
+        }
+        return res;
+    }
     // choose base B random to avoid hacks 33 37 41
     // randomize V(s[i])
-    int B = 1777771, M[2] = {999727999, 1070777777}, P[2] = {325255434, 10018302};
-    vl H[2], I[2];
-    RH(string &s){
-        int N = s.size(); rep(k, 2){
+    const int B = 119, M[2] = {999727999, 1070777777}, P[2] = {325255434, 10018302};
+    vector<int> H[2], I[2];
+    RH(string &s) {
+        int N = s.size(); 
+        rep(k, 2) {
             H[k].resize(N + 1), I[k].resize(N + 1);
-            H[k][0] = 0, I[k][0] = 1; ll b = 1;
-            rep(i, N + 1) if (i){
-                H[k][i] = (H[k][i - 1] + b * s[i - 1]) % M[k];
-                I[k][i] = (1LL * I[k][i - 1] * P[k]) % M[k];
+            H[k][0] = 0; ll b = 1;
+            rep(i, N) {
+                H[k][i + 1] = (H[k][i] + b * (s[i] - 'a' + 1)) % M[k];
                 b = (b * B) % M[k];
             }
+            I[k][N] = binpow(b, M[k] - 2, M[k]);
+            for(int i = N - 1; i >= 0; i--) I[k][i] = (1LL * I[k][i + 1] * B) % M[k]; 
         }
     }
-    ll get(int l, int r){ // inclusive - exclusive
+    ll get(int l, int r)  // inclusive - exclusive
+    {
         ll h0 = (H[0][r] - H[0][l] + M[0]) % M[0];
         h0 = (1LL * h0 * I[0][l]) % M[0];
-        ll h1 = (H[1][r] - H[1][r] + M[1]) % M[1];
+        ll h1 = (H[1][r] - H[1][l] + M[1]) % M[1];
         h1 = (1LL * h1 * I[1][l]) % M[1];
         return (h0 << 32) | h1;
     }
